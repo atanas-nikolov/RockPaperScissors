f6741097d185f9cae1d18830eacdf43414afe7d1
atanas-nikolov <master89q@gmail.com>
2024-09-10 18:05:25 +0300

    add validation to check user input

--- app/src/main/java/com/nikolovfactory/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/nikolovfactory/rockpaperscissors/RockPaperScissors.kt
@@ -7,38 +7,46 @@ fun main () {
     println("Rock, Paper or Scissors? Enter you choice!")
 
     playerChoice = readln()
-    println("Player choose : " + playerChoice)
-    // here we create a computer logic
-    val randomNumber = (1..3).random()
 
-    when (randomNumber) {
-        1 -> {
-            computerChoice = "Rock"
-            //println("Rock")
-        }
-        2 -> {
-            computerChoice = "Paper"
-            //println("Paper")
-        }
-        3 -> {
-            computerChoice = "Scissors"
-            // println("Scissors")
-        }
-    }
-    println("Computer choose : " + computerChoice)
+    while(playerChoice == "Rock" || playerChoice == "Paper" || playerChoice == "Scissors") {
 
-    val winner = when {
-        playerChoice == computerChoice -> "Tie"
-        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
-        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
-        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
-        else -> "Computer"
+        println("Player choose : " + playerChoice)
+        // here we create a computer logic
+        val randomNumber = (1..3).random()
+
+        when (randomNumber) {
+            1 -> {
+                computerChoice = "Rock"
+                //println("Rock")
+            }
+            2 -> {
+                computerChoice = "Paper"
+                //println("Paper")
+            }
+            3 -> {
+                computerChoice = "Scissors"
+                // println("Scissors")
+            }
+        }
+        println("Computer choose : " + computerChoice)
+
+        val winner = when {
+            playerChoice == computerChoice -> "Tie"
+            playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+            playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+            playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+            else -> "Computer"
+        }
+        if (winner == "Tie") {
+            println("Tie, no winner!")
+        } else {
+            println("$winner won!")
+        }
+        playerChoice = readln()
     }
-    if (winner == "Tie") {
-        println("Tie, no winner!")
-    } else {
-        println("$winner won!")
-    }
+    println("Еrror! " +"\n" + "Please enter one of the given words and spell it correctly!")
+
+
 
 
 }
